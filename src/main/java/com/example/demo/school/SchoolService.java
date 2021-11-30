cb01e8685dde818e4ab6cc9c5c9a2df7e58be2f9
LennyDennis <lennydennis@gmail.com>
2021-11-30 21:49:22 +0300

    Add a school repository

--- src/main/java/com/example/demo/school/SchoolService.java
+++ src/main/java/com/example/demo/school/SchoolService.java
@@ -1,5 +1,6 @@
 package com.example.demo.school;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,7 +10,14 @@ import java.util.List;
 @Service
 public class SchoolService {
 
+    private SchoolRepository schoolRepository;
+
+    @Autowired
+    public SchoolService(SchoolRepository schoolRepository) {
+        this.schoolRepository = schoolRepository;
+    }
+
     public List<School> getSchool(){
-        return Arrays.asList(new School(1,"Test School"));
+        return schoolRepository.findAll();
     }
 }
